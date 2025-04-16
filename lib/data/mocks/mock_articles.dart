4829726e8578472dc21b22d7a8d07b5964f09d0c
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-04-15 22:51:00 -0300

    chore: organize folder structure and add article mock/model

---
+++ lib/data/mocks/mock_articles.dart
@@ -0,0 +1,16 @@
+import '../models/article_model.dart';
+
+final List<Article> mockArticles = [
+  Article(
+    image: 'https://www.allrecipes.com/thmb/K9Ea4w7sUDgfgMfRLbLopR7oxok=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/50050-five-minute-ice-cream-DDMFS-4x3-076-fbf49ca6248e4dceb3f43a4f02823dd9.jpg',
+    title: 'Ice cream is made with carrageenan ...',
+  ),
+  Article(
+    image: 'https://as1.ftcdn.net/jpg/01/98/46/60/1000_F_198466056_PCqEL4oinf9DEbHurjpS7BvW2vRmjMhy.jpg',
+    title: 'Is makeup one of your daily esse ...',
+  ),
+  Article(
+    image: 'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG',
+    title: 'Coffee is more than just a drink: It\'s ...',
+  ),
+];
---
+++ lib/data/models/article_model.dart
@@ -0,0 +1,6 @@
+class Article {
+  final String image;
+  final String title;
+
+  Article({required this.image, required this.title});
+}
