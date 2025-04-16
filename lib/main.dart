aa2410d58dbb02d1997dcb4ebe457ebc40faeb1a
Arthur <arthur_de_faria@outlook.com>
2025-04-15 21:59:27 -0300

    feat: setup home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Teste'),
-        ),
-      ),
+    return MaterialApp(
+      home: HomePage()
     );
   }
 }
